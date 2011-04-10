1c6af23b3df843e7a15adc1394c6b8908e3d27f7
Horia Coman <horia141@gmail.com>
2011-04-10 13:40:37 +0300

    Improved the game of life.
    * I've created the "gol_data" object to handle all the cellular automaton
      stuff (evolving, accessing data, reading from a file). It's basically
      similar to an image but with an evolve function instead of the copy one.
    
    * I no longer rely on a tquad's texture to store data. Instead we use two
      gol_data objects and write the current generation to the texture at
      display time. This yields a much cleaner core, as we don't have to rely
      on tquads for our storage (that's not their role).
    
    * I've also added 12 initial patterns useful for quick examples.

--- Makefile
+++ Makefile
@@ -4,14 +4,20 @@ GOL_SRC_C = game_of_life.c
 GOL_SRC_H =
 GOL_SRC = $(GOL_SRC_C) $(GOL_SRC_H)
 
-gol-cpu: $(GOL_SRC)
-	gcc -g -Wall -o gol-cpu -DCPU $(GOL_SRC_C) -L./lib -lsnt
+gol-cpu-debug: $(GOL_SRC)
+	gcc -g -Wall -o gol-cpu-debug -DCPU $(GOL_SRC_C) -L./lib -lsnt.debug
+
+gol-cpu-final: $(GOL_SRC)
+	gcc -O2 -DNDEBUG -Wall -o gol-cpu-final -DCPU $(GOL_SRC_C) -L./lib -lsnt
 
 gol-gpu: $(SRC_C) $(SRC_H)
 	nvcc -g -G -o gol-cpu -DCPU $(GOL_SRC_C) -L./lib -lsnt
 
-gol-cpu-run: gol-cpu
-	./gol-cpu
+gol-cpu-debug-run: gol-cpu-debug
+	./gol-cpu-debug
+
+gol-cpu-final-run: gol-cpu-final
+	./gol-cpu-final
 
 gol-gpu-run: gol-gpu
 	./gol-gpu
--- game_of_life.c
+++ game_of_life.c
@@ -10,125 +10,299 @@
 #include "ext/snt/image.h"
 #include "ext/snt/driver.h"
 
+#define ALIVE 1
+#define DEAD 0
+
+typedef struct _gol_data  gol_data;
+
 static struct {
   int     max_iteration;
   int     ms_per_frame;
-  image*  initial_state;
+  char*   life_path;
   bool    exit_on_stop;
 } config;
 
 static struct {
-  int      curr_iteration;
-  int      show_which;
-  driver*  drv;
-  tquad*   gol_data0;
-  tquad*   gol_data1;
+  int        curr_iteration;
+  int        show_which;
+  gol_data*  gol_data0;
+  gol_data*  gol_data1;
+  int        rows;
+  int        cols;
+  driver*    drv;
+  tquad*     tq;
 } state;
 
+struct _gol_data
+{
+  int  rows;
+  int  cols;
+  int  data[];
+};
+
+static gol_data*  gol_data_from_l(const char* path);
+static void       gol_data_free(gol_data* gol);
+
+static bool       gol_data_is_valid(const gol_data* gol);
+
+static gol_data*  gol_data_evolve(gol_data* dst, const gol_data* src);
+static int        gol_data_neigh_count(const gol_data* gol, int i, int j);
+static int        gol_data_get_rows(const gol_data* gol);
+static int        gol_data_get_cols(const gol_data* gol);
+static int        gol_data_get(const gol_data* gol, int i, int j);
+static void       gol_data_set(gol_data* gol, int i, int j, int cell_state);
+
+static gol_data* 
+gol_data_from_l(
+  const char* path)
+{
+  assert(path != NULL);
+  assert(path[0] != '\0');
+
+  FILE*      l_file;
+  gol_data*  new_gol;
+  int        rows;
+  int        cols;
+  bool       know_cols;
+  int        c_char;
+  int        d_ptr;
+
+  rows = 0;
+  cols = 0;
+  know_cols = false;
+
+  l_file = fopen(path,"rt");
+
+  while ((c_char = fgetc(l_file)) != EOF) {
+    if (c_char == '\n') {
+      rows += 1;
+      know_cols = true;
+    } else {
+      if (!know_cols) {
+	cols += 1;
+      }
+    }
+  }
+
+  new_gol = malloc(sizeof(gol_data) + sizeof(int) * rows * cols);
+
+  new_gol->rows = rows;
+  new_gol->cols = cols;
+
+  fseek(l_file,0,SEEK_SET);
+
+  d_ptr = 0;
+
+  while ((c_char = fgetc(l_file)) != EOF) {
+    if (c_char == '.') {
+      new_gol->data[d_ptr] = DEAD;
+      d_ptr += 1;
+    } else if (c_char == 'X') {
+      new_gol->data[d_ptr] = ALIVE;
+      d_ptr += 1;
+    } else if (c_char == '\n') {
+    } else {
+      assert(0);
+    }
+  }
+
+  fclose(l_file);
+
+  return new_gol;
+}
+
+static void
+gol_data_free(
+  gol_data* gol)
+{
+  assert(gol_data_is_valid(gol));
+
+  gol->rows = -1;
+  gol->cols = -1;
+
+  free(gol);
+}
+
+static bool
+gol_data_is_valid(
+  const gol_data* gol)
+{
+  int  i;
+  int  j;
+
+  if (gol == NULL) {
+    return false;
+  }
+
+  if (gol->rows < 1) {
+    return false;
+  }
+
+  if (gol->cols < 1) {
+    return false;
+  }
+
+  for (i = 0; i < gol->rows; i++) {
+    for (j = 0; j < gol->cols; j++) {
+      if (gol->data[i * gol->cols + j] != ALIVE &&
+	  gol->data[i * gol->cols + j] != DEAD) {
+	return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+static gol_data*
+gol_data_evolve(
+  gol_data* dst,
+  const gol_data* src)
+{
+  assert(gol_data_is_valid(dst));
+  assert(gol_data_is_valid(src));
+  assert(dst->rows == src->rows);
+  assert(dst->cols == src->cols);
+
+  int  count;
+  int  i;
+  int  j;
+
+  for (i = 0; i < dst->rows; i++) {
+    for (j = 0; j < dst->cols; j++) {
+      count = gol_data_neigh_count(src,i,j);
+
+      if (gol_data_get(src,i,j) == ALIVE) {
+	if (count < 2) {
+	  gol_data_set(dst,i,j,DEAD);
+	} else if (count == 2 || count == 3) {
+	  gol_data_set(dst,i,j,ALIVE);
+	} else {
+	  gol_data_set(dst,i,j,DEAD);
+	}
+      } else {
+	if (count == 3) {
+	  gol_data_set(dst,i,j,ALIVE);
+	} else {
+	  gol_data_set(dst,i,j,DEAD);
+	}
+      }
+    }
+  }
+
+  return dst;
+}
+
 static int
-gol_count_live_neighbours(
-  tquad* tq,
+gol_data_neigh_count(
+  const gol_data* gol,
+  int  i,
+  int  j)
+{
+  assert(gol_data_is_valid(gol));
+  assert(i >= 0 && i < gol->rows);
+  assert(j >= 0 && j < gol->cols);
+
+  int  ip1;
+  int  im1;
+  int  jp1;
+  int  jm1;
+
+  ip1 = (i != gol->rows - 1) ? (i + 1) : 0;
+  im1 = (i != 0) ? (i - 1) : (gol->rows - 1);
+  jp1 = (j != gol->cols - 1) ? (j + 1) : 0;
+  jm1 = (j != 0) ? (j - 1) : (gol->cols - 1);
+
+  return (gol_data_get(gol,im1,j) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,im1,jp1) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,i,jp1) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,ip1,jp1) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,ip1,j) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,ip1,jm1) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,i,jm1) == ALIVE ? 1 : 0) +
+         (gol_data_get(gol,im1,jm1) == ALIVE ? 1 : 0);
+}
+
+static int
+gol_data_get_rows(
+  const gol_data* gol)
+{
+  assert(gol_data_is_valid(gol));
+  
+  return gol->rows;
+}
+
+static int
+gol_data_get_cols(
+  const gol_data* gol)
+{
+  assert(gol_data_is_valid(gol));
+  
+  return gol->cols;
+}
+
+static int
+gol_data_get(
+  const gol_data* gol,
   int i,
   int j)
 {
-  assert(tquad_is_valid(tq));
-  assert(i >= 1 && i < image_get_rows(tquad_get_texture(tq)) - 1);
-  assert(j >= 1 && j < image_get_rows(tquad_get_texture(tq)) - 1);
+  assert(gol_data_is_valid(gol));
+  assert(i >= 0 && i < gol->rows);
+  assert(j >= 0 && j < gol->cols);
 
-  int    count = 0;
-  color  alive = color_make_rgb(0,0,0);
+  return gol->data[i * gol->cols + j];
+}
 
-  if (color_equal(tquad_texture_get(tq,i-1,j),&alive)) {
-    count += 1;
-  }
+static void
+gol_data_set(
+  gol_data* gol,
+  int i,
+  int j,
+  int cell_state)
+{
+  assert(gol_data_is_valid(gol));
+  assert(i >= 0 && i < gol->rows);
+  assert(j >= 0 && j < gol->cols);
+  assert(cell_state == ALIVE || cell_state == DEAD);
 
-  if (color_equal(tquad_texture_get(tq,i-1,j+1),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i,j+1),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i+1,j+1),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i+1,j),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i+1,j-1),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i,j-1),&alive)) {
-    count += 1;
-  }
-
-  if (color_equal(tquad_texture_get(tq,i-1,j-1),&alive)) {
-    count += 1;
-  }
-
-  return count;
+  gol->data[i * gol->cols + j] = cell_state;
 }
 
 static int
 gol_frame_cb()
 {
   if (state.curr_iteration < config.max_iteration) {
-    tquad*  prev;
-    tquad*  curr;
-    color   dead = color_make_rgb(1,1,1);
-    color   alive = color_make_rgb(0,0,0);
-    int     rows;
-    int     cols;
-    int     count;
-    int     i;
-    int     j;
+    gol_data*  prev;
+    gol_data*  curr;
+    int        i;
+    int        j;
 
+    fprintf(stderr,"Iteration #%d\n",state.curr_iteration + 1);
+    
     if (state.show_which == 0) {
       prev = state.gol_data0;
       curr = state.gol_data1;
+      state.show_which = 1;
     } else {
       prev = state.gol_data1;
       curr = state.gol_data0;
+      state.show_which = 0;
     }
 
-    rows = image_get_rows(tquad_get_texture(prev));
-    cols = image_get_cols(tquad_get_texture(prev));
+    gol_data_evolve(curr,prev);
 
-    for (i = 1; i < rows-1; i++) {
-      for (j = 1; j < cols-1; j++) {
-	count = gol_count_live_neighbours(prev,i,j);
-
-	if (color_equal(tquad_texture_get(prev,i,j),&alive)) {
-	  if (count < 2) {
-	    tquad_texture_set(curr,i,j,&dead);
-	  } else if (count == 2 || count == 3) {
-	    tquad_texture_set(curr,i,j,&alive);
-	  } else {
-	    tquad_texture_set(curr,i,j,&dead);
-	  }
+    for (i = 0; i < state.rows; i++) {
+      for (j = 0; j < state.cols; j++) {
+	if (gol_data_get(curr,i,j) == ALIVE) {
+	  tquad_texture_set(state.tq,i,j,&(color){0,0,0,1});
 	} else {
-	  if (count == 3) {
-	    tquad_texture_set(curr,i,j,&alive);
-	  } else {
-	    tquad_texture_set(curr,i,j,&dead);
-	  }
+	  tquad_texture_set(state.tq,i,j,&(color){1,1,1,1});
 	}
       }
     }
 
-    tquad_hide(prev);
-    tquad_show(curr);
-
-    if (state.show_which == 0) {
-      state.show_which = 1;
-    } else {
-      state.show_which = 0;
-    }
-    
     state.curr_iteration += 1;
 
     return 1;
@@ -146,23 +320,25 @@ main(
   int argc,
   char** argv)
 {
-  config.max_iteration = 1000;
-  config.ms_per_frame = 250;
-  config.initial_state = image_from_ppm_t("gol4.ppm");
-  config.exit_on_stop = false;
+  config.max_iteration = 10000;
+  config.ms_per_frame = 10;
+  config.life_path = "data/life12.l";
+  config.exit_on_stop = true;
 
   state.curr_iteration = 0;
   state.show_which = 0;
+  state.gol_data0 = gol_data_from_l(config.life_path);
+  state.gol_data1 = gol_data_from_l(config.life_path);
+  state.rows = gol_data_get_rows(state.gol_data0);
+  state.cols = gol_data_get_cols(state.gol_data1);
   state.drv = driver_make(gol_frame_cb,config.ms_per_frame);
-  state.gol_data0 = driver_tquad_make_image(state.drv,&(rectangle){0,0,1,1},config.initial_state);
-  state.gol_data1 = driver_tquad_make_copy(state.drv,state.gol_data0);
-
-  tquad_hide(state.gol_data1);
+  state.tq = driver_tquad_make_color(state.drv,&(rectangle){0,0,1,1},state.rows,state.cols,&(color){1,1,1,1});
 
   driver_start(state.drv);
 
   driver_free(state.drv);
-  image_free(config.initial_state);
+  gol_data_free(state.gol_data0);
+  gol_data_free(state.gol_data1);
 
   return 0;
 }
